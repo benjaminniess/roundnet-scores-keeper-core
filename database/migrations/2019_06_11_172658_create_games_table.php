e616456773c5caf6ad6388b6a2145797c9925b34
Benjamin Niess <benjamin.niess@gmail.com>
2019-06-11 21:23:20 +0200

    Added a basic game model + migration

---
+++ database/migrations/2019_06_11_172658_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('player1');
+            $table->string('player2');
+            $table->string('player3');
+            $table->string('player4');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}

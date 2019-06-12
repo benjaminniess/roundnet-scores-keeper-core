7007041586e9f2310c15d930f94771e069e2ffba
Benjamin Niess <bniess@sevanova.com>
2019-06-12 14:01:45 +0200

    Worked on games base structure

--- database/migrations/2019_06_11_172658_create_games_table.php
+++ database/migrations/2019_06_11_172658_create_games_table.php
@@ -13,15 +13,40 @@ class CreateGamesTable extends Migration
      */
     public function up()
     {
+	    Schema::create('actions_types', function (Blueprint $table) {
+		    $table->bigIncrements('id');
+		    $table->string('name');
+		    $table->string('action_type');
+		    $table->timestamps();
+	    });
+
         Schema::create('games', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
-            $table->string('player1');
-            $table->string('player2');
-            $table->string('player3');
-            $table->string('player4');
+            $table->integer('player1');
+            $table->integer('player2');
+            $table->integer('player3');
+            $table->integer('player4');
+            $table->integer('score_team_1');
+            $table->integer('score_team_2');
+            $table->integer('game_duration');
             $table->timestamps();
+
+	        $table->foreign('player1')->references('id')->on('users');
+	        $table->foreign('player2')->references('id')->on('users');
+	        $table->foreign('player3')->references('id')->on('users');
+	        $table->foreign('player4')->references('id')->on('users');
         });
+
+	    Schema::create('game_points', function (Blueprint $table) {
+		    $table->bigIncrements('id');
+		    $table->integer('player_id');
+		    $table->integer('action_type_id');
+		    $table->integer('score_team_1');
+		    $table->integer('score_team_2');
+		    $table->timestamps();
+
+		    $table->foreign('action_type_id')->references('id')->on('actions_types');
+	    });
     }
 
     /**
@@ -31,6 +56,8 @@ class CreateGamesTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('actions_types');
         Schema::dropIfExists('games');
+	    Schema::dropIfExists('game_points');
     }
 }

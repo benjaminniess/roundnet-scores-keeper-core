bbc7e2d8fc7c1c56562071f5e63b7bbd0bb4fea9
Benjamin Niess <benjamin.niess@gmail.com>
2019-06-12 19:58:07 +0200

    Added relationship table

--- database/migrations/2019_06_11_172658_create_games_table.php
+++ database/migrations/2019_06_11_172658_create_games_table.php
@@ -13,12 +13,13 @@ class CreateGamesTable extends Migration
      */
     public function up()
     {
-	    Schema::create('actions_types', function (Blueprint $table) {
-		    $table->bigIncrements('id');
-		    $table->string('name');
-		    $table->string('action_type');
-		    $table->timestamps();
-	    });
+
+        Schema::create('actions_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('action_type');
+            $table->timestamps();
+        });
 
         Schema::create('games', function (Blueprint $table) {
             $table->bigIncrements('id');
@@ -31,22 +32,35 @@ class CreateGamesTable extends Migration
             $table->integer('game_duration');
             $table->timestamps();
 
-	        $table->foreign('player1')->references('id')->on('users');
-	        $table->foreign('player2')->references('id')->on('users');
-	        $table->foreign('player3')->references('id')->on('users');
-	        $table->foreign('player4')->references('id')->on('users');
+            $table->foreign('player1')->references('id')->on('users');
+            $table->foreign('player2')->references('id')->on('users');
+            $table->foreign('player3')->references('id')->on('users');
+            $table->foreign('player4')->references('id')->on('users');
         });
 
-	    Schema::create('game_points', function (Blueprint $table) {
-		    $table->bigIncrements('id');
-		    $table->integer('player_id');
-		    $table->integer('action_type_id');
-		    $table->integer('score_team_1');
-		    $table->integer('score_team_2');
-		    $table->timestamps();
+        Schema::create('game_points', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('player_id');
+            $table->integer('action_type_id');
+            $table->integer('score_team_1');
+            $table->integer('score_team_2');
+            $table->timestamps();
 
-		    $table->foreign('action_type_id')->references('id')->on('actions_types');
-	    });
+            $table->foreign('action_type_id')->references('id')->on('actions_types');
+        });
+
+
+        Schema::create('user_relationships', function (Blueprint $table) {
+            $table->integer('user_id_1');
+            $table->integer('user_id_2');
+            $table->integer('status');
+            $table->timestamps();
+
+            $table->foreign('user_id_1')->references('id')->on('users');
+            $table->foreign('user_id_2')->references('id')->on('users');
+
+            $table->primary(['user_id_1', 'user_id_2']);
+        });
     }
 
     /**
@@ -58,6 +72,7 @@ class CreateGamesTable extends Migration
     {
         Schema::dropIfExists('actions_types');
         Schema::dropIfExists('games');
-	    Schema::dropIfExists('game_points');
+        Schema::dropIfExists('game_points');
+        Schema::dropIfExists('user_relationships');
     }
 }

fb714f220cfd3f06529859ab626554123cc855b1
CHARLEY GUENIER <charley.guenier@gmail.com>
2019-06-16 00:06:52 +0200

    Add games

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use \App\Game;
+use \App\User;
 use Illuminate\Support\Facades\Auth;
 
 class GamesController extends Controller
@@ -47,8 +48,8 @@ class GamesController extends Controller
   // CREATE A NEW PROJECT
   function create()
   {
-
-    return view('games.create');
+      $players = User::all();
+      return view('games.create',compact('players'));
   }
 
   function store()
--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <h2>Create a new game</h2>
-
 <form class="form" action="/games" method="POST">
 
 
@@ -10,35 +9,39 @@
     <h3>Team 1</h3>
         <label for="player1">Player 1</label>
         <select class="select" name="player1">
-            <option value="1">Charley</option>
-            <option value="2">Axel</option>
-            <option value="3">Bniess</option>
-            <option value="4">Sleps</option>
+            @foreach($players as $player)
+
+                <option value="{{ $player->id }}">{{ $player->name }}</option>
+
+            @endforeach
         </select>
 
         <label for="player2">Player 2</label>
         <select class="select" name="player2">
-            <option value="1">Charley</option>
-            <option value="2">Axel</option>
-            <option value="3">Bniess</option>
-            <option value="4">Sleps</option>
+            @foreach($players as $player)
+
+                <option value="{{ $player->id }}">{{ $player->name }}</option>
+
+            @endforeach
         </select>
 
-        <h3>Team 2</h3>
-            <label for="player3">Player 3</label>
-            <select class="select" name="player3">
-                <option value="1">Charley</option>
-                <option value="2">Axel</option>
-                <option value="3">Bniess</option>
-                <option value="4">Sleps</option>
-            </select>
+    <h3>Team 2</h3>
+        <label for="player3">Player 3</label>
+        <select class="select" name="player3">
+            @foreach($players as $player)
+
+                <option value="{{ $player->id }}">{{ $player->name }}</option>
+
+            @endforeach
+        </select>
 
         <label for="player4">Player 4</label>
         <select class="select" name="player4">
-            <option value="1">Charley</option>
-            <option value="2">Axel</option>
-            <option value="3">Bniess</option>
-            <option value="4">Sleps</option>
+            @foreach($players as $player)
+
+                <option value="{{ $player->id }}">{{ $player->name }}</option>
+
+            @endforeach
         </select>
 
         <button type="submit"> envoyer</button>

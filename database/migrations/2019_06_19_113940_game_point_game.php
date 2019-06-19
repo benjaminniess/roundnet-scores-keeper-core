ba4d9d1385c3e46bdb612ef90f0186ef53b7596d
Benjamin Niess <bniess@sevanova.com>
2019-06-19 13:48:58 +0200

    Added migration file for game points related game

---
+++ database/migrations/2019_06_19_113940_game_point_game.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GamePointGame extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::table('game_points', function (Blueprint $table) {
+		    $table->integer('game_id')->nullable();
+
+		    $table->foreign('game_id')->references('id')->on('games');
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::table('game_points', function (Blueprint $table) {
+		    $table->dropColumn('game_id');
+	    });
+    }
+}

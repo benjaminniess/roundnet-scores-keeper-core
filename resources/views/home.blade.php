7cb218cffcffc09ae2c123280c6c2732fcdac147
Benjamin Niess <benjamin.niess@gmail.com>
2019-06-30 21:19:43 +0200

    Added some buttons to logged in HP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,13 @@
 
 @section('content')
 
-    <h2>Home</h2>
-
     <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
+        <p>You don't have any live game yet.</p>
 
+        <div class="">
+            <a class="btn btn-info btn-lg m-3" href="/games/create">Start a new game</a>
+            <a class="btn btn-info btn-lg m-3" href="/friends">Search for friends</a>
+        </div>
     </div>
 @endsection
 

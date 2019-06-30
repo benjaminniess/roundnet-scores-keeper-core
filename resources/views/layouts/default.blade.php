f882ddc80b208988a0935a837ad4a650a3d91498
Benjamin Niess <benjamin.niess@gmail.com>
2019-06-30 22:01:58 +0200

    Added powered by LSC footer

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -23,5 +23,10 @@
         @yield('content')
     </div>
 </div>
+<footer class="footer mt-auto py-3">
+    <div class="container">
+        <span class="text-muted">Powered by <a href="https://www.instagram.com/lyonspikeballclub/">LSC</a>.</span>
+    </div>
+</footer>
 </body>
 </html>

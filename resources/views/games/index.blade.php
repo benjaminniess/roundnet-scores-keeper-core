213c216eea6131efd17fae672eacc7f5699a5c39
Chly <charley.guenier@gmail.com>
2019-07-02 00:26:19 +0200

    Display games with 3 items per rows + change badge style depending on game status

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -2,51 +2,57 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col">
+
             <h1 class="heading">Games list</h1>
-        </div>
-    </div>
 
-<div class="row mt-3 mb-5">
     @if (!$games->isEmpty())
-        @foreach($games as $game)
-            <div class="col-sm">
-                <div class="card text-center">
-                    <div class="card-header">
-                        <span class="badge badge-success">{{ $game->status }}</span>
-                    </div>
+    @foreach ($games->chunk(3) as $gamesChunked)
+        <div class="row my-3">
+            @foreach($gamesChunked as $game)
+                <div class="col-sm-4">
+                    <div class="card text-center">
+                        <div class="card-header">
+                            <span class="badge 
+                                @if ($game->status == 'pending')
+                                    {{ 'badge-dark' }}
+                                @endif
+                                @if ($game->status == 'live')
+                                    {{ 'badge-success' }}
+                                @endif
+                            ">{{ $game->status }}</span>
+                        </div>
 
-                    <div class="card-body">
-                        <h2 class="card-title">Players</h2>
-                            <ul class="list-group list-group-flush">
-                                @foreach( $game->players as $player)
-                                <li class="list-group-item">{{ $player->name }}</li>
-                                @endforeach
-                            </ul>
-                            <div class="row mt-3">
-                                <div class="col-md-6 offset-md-3">
-                                    @if ( $game->status == 'pending' )
-                                        <a href="{{ url('/games/' . $game->id . '/start') }}" class="btn btn-success"> Start </a>
-                                    @else
-                                        <a href="{{ url('/games') }}/{{ $game->id }}" class="btn btn-info"> View </a>
-                                    @endif
-                                        <a href="{{ url('/games') }}/{{ $game->id }}/edit" class="btn btn-primary"> Edit </a>
+                        <div class="card-body">
+                            <h2 class="card-title">Players</h2>
+                                <ul class="list-group list-group-flush">
+                                    @foreach( $game->players as $player)
+                                    <li class="list-group-item">{{ $player->name }}</li>
+                                    @endforeach
+                                </ul>
+                                <div class="row mt-3">
+                                    <div class="col-md-6 offset-md-3">
+                                        @if ( $game->status == 'pending' )
+                                            <a href="{{ url('/games/' . $game->id . '/start') }}" class="btn btn-success"> Start </a>
+                                        @else
+                                            <a href="{{ url('/games') }}/{{ $game->id }}" class="btn btn-info"> View </a>
+                                        @endif
+                                            <a href="{{ url('/games') }}/{{ $game->id }}/edit" class="btn btn-primary"> Edit </a>
+                                    </div>
                                 </div>
-                            </div>
+                        </div>
+
+                        <div class="card-footer text-muted"> {{ $game->get_date() }} </div>
+
                     </div>
-
-                    <div class="card-footer text-muted"> {{ $game->get_date() }} </div>
-
                 </div>
-            </div>
-        @endforeach
-</div>
+            @endforeach
+        </div>
+    @endforeach
 
     @else {{ 'there is no game' }}
     @endif
     
-    <div class="row">
+    <div class="row my-3">
         <div class="col-md-6 offset-md-3">
             <a href="{{ url('/games/create') }}" class="btn-lg btn-block btn-primary"> Add a new game </a>
         </div>

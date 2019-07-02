5a76802784a91b95cbb193eb6f1198b6c18d7722
Chly <charley.guenier@gmail.com>
2019-07-02 13:14:30 +0200

    Add score in game list

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="container">
 
-            <h1 class="heading">Games list</h1>
+            <h1 class="heading mb-4">Games list</h1>
 
     @if (!$games->isEmpty())
-    @foreach ($games->chunk(3) as $gamesChunked)
-        <div class="row my-3">
-            @foreach($gamesChunked as $game)
-                <div class="col-sm-4">
+    @foreach ($games->chunk(2) as $gamesRow)
+        <div class="row my-2">
+            @foreach($gamesRow as $game)
+                <div class="col-sm-6 my-2">
                     <div class="card text-center">
                         <div class="card-header">
                             <span class="badge 
@@ -23,12 +23,26 @@
                         </div>
 
                         <div class="card-body">
-                            <h2 class="card-title">Players</h2>
-                                <ul class="list-group list-group-flush">
-                                    @foreach( $game->players as $player)
-                                    <li class="list-group-item">{{ $player->name }}</li>
-                                    @endforeach
-                                </ul>
+                            <div class="row">
+                                <div class="col-sm-6 col-6">
+                                    <h2 class="card-title">Team A</h2>
+                                    <ul class="list-group list-group-flush">
+                                        @foreach($game->players->chunk(2)[0] as $player)
+                                        <li class="list-group-item">{{ $player->name }}</li>
+                                        @endforeach
+                                    </ul>
+                                    <h2 class="heading">{{ $game->score_team_1 }}</h2>
+                                </div>
+                                <div class="col-sm-6 col-6">
+                                    <h2 class="card-title">Team B</h2>
+                                        <ul class="list-group list-group-flush">
+                                            @foreach($game->players->chunk(2)[1] as $player)
+                                            <li class="list-group-item">{{ $player->name }}</li>
+                                            @endforeach
+                                        </ul>
+                                        <h2 class="heading">{{ $game->score_team_2 }}</h2>
+                                </div>
+                            </div>
                                 <div class="row mt-3">
                                     <div class="col-md-6 offset-md-3">
                                         @if ( $game->status == 'pending' )

3d1405ab1b6c629eb17b388e55afead33ce3a520
Chly <charley.guenier@gmail.com>
2019-07-03 13:35:02 +0200

    Clean authentification pages style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="row justify-content-center">
+<div class="row justify-content-center my-5">
     <div class="col-md-8">
         <div class="card">
             <div class="card-header">{{ __('Register') }}</div>

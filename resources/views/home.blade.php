9acc69ebbbe67974435bf25e47ee644e8fc94c71
Chly <charley.guenier@gmail.com>
2019-07-03 19:33:59 +0200

    Design homepage when no game live

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
     <div class="card-body">
-        <p>You don't have any live game yet.</p>
+        <div class=" alert alert-info">You don't have any live game yet.</div>
 
         <div class="">
-            <a class="btn btn-info btn-lg m-3" href="/games/create">Start a new game</a>
-            <a class="btn btn-info btn-lg m-3" href="/friends">Search for friends</a>
+            <a class="btn btn-primary" href="/games/create">Start a new game</a>
+            <a class="btn btn-primary" href="/friends">Search for friends</a>
         </div>
     </div>
 @endsection

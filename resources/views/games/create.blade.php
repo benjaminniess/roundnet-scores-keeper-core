8c4b4d07a346472cb461beb35fc43c96fe2d83f2
Chly <charley.guenier@gmail.com>
2019-07-03 19:00:21 +0200

    Redesign add game form

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -6,78 +6,111 @@
 
 <form action="/games" method="POST">
     @csrf
-        <h3 class="heading my-3">Team 1</h3>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="player1">Player 1</label>
-                <select class="form-control" name="player1" required>
-                    <option value="">Select a player</option>
-                    @foreach($players as $player)
 
-                        <option @php echo old('player1') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
+<div class="row my-2">
+    <div class="col-sm-6 my-2">
+      <div class="card">
+          <h5 class="card-header">Team 1</h5>
+          <div class="card-body">
+                <div class="form-group">
+                    <label for="player1">Player 1</label>
+                    <select class="form-control" name="player1" required>
+                        <option value="">Select a player</option>
+                        @foreach($players as $player)
 
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group col-md-6">
-                <label for="player2">Player 2</label>
-                <select class="form-control" name="player2" required>
-                    <option value="">Select a player</option>
-                    @foreach($players as $player)
+                            <option @php echo old('player1') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
 
-                        <option @php echo old('player2') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
 
-                    @endforeach
-                </select>
+                <div class="form-group">
+                    <label for="player2">Player 2</label>
+                    <select class="form-control" name="player2" required>
+                        <option value="">Select a player</option>
+                        @foreach($players as $player)
+
+                            <option @php echo old('player2') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
+
+                        @endforeach
+                    </select>
+                </div>
+          </div>
+        </div>  
+    </div>
+
+    <div class="col-sm-6 my-2">
+        <div class="card">
+            <h5 class="card-header">Team 2</h5>          
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="player3">Player 3</label>
+                        <select class="form-control" name="player3" required>
+                            <option value="">Select a player</option>
+                            @foreach($players as $player)
+
+                                <option @php echo old('player3') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
+
+                            @endforeach
+                        </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="player4">Player 4</label>
+                    <select class="form-control" name="player4" required>
+                        <option value="">Select a player</option>
+                        @foreach($players as $player)
+
+                            <option @php echo old('player4') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
+
+                        @endforeach
+                    </select>
+                </div>
             </div>
         </div>
+    </div>
+</div>
 
-    <h3 class="heading my-3">Team 2</h3>
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <label for="player3">Player 3</label>
-                <select class="form-control" name="player3" required>
-                    <option value="">Select a player</option>
-                    @foreach($players as $player)
+<div class="row my-2">
+    <div class="col-sm-6 my-2">
+        <div class="card">
+            <h5 class="card-header">Options</h5>          
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="referee">Referee</label>
+                    <select class="form-control" name="referee">
+                        <option value="">Select a referee if necessary</option>
+                        @foreach($players as $player)
 
-                        <option @php echo old('player3') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
+                            <option @php echo old('referee') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
 
-                    @endforeach
-                </select>
-        </div>
-        <div class="form-group col-md-6">
-            <label for="player4">Player 4</label>
-            <select class="form-control" name="player4" required>
-                <option value="">Select a player</option>
-                @foreach($players as $player)
-
-                    <option @php echo old('player4') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
-
-                @endforeach
-            </select>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="game_points">points to win</label>
+                    <input type="number" name="points_to_win" id="game_points" class="form-control" required value="{{ old('points_to_win', 21) }}">
+                </div>
+            </div>
         </div>
     </div>
 
-    <h3 class="heading my-3">Options</h3>
-
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <label for="referee">Referee</label>
-            <select class="form-control" name="referee">
-                <option value="">Select a referee if necessary</option>
-                @foreach($players as $player)
-
-                    <option @php echo old('referee') == $player->id ? 'selected' : '' @endphp value="{{ $player->id }}">{{ $player->name }}</option>
-
-                @endforeach
-            </select>
-        </div>
-
-        <div class="form-group col-md-6">
-            <label for="game_points">points to win</label>
-            <input type="number" name="points_to_win" id="game_points" class="form-control" required value="{{ old('points_to_win', 21) }}">
+    <div class="col-sm-6 my-2">
+        <div class="card">
+            <h5 class="card-header">Start game</h5>          
+            <div class="card-body">
+                <div class="form-group form-check">
+                    <input type="checkbox" name="start_now" id="start_now" class="form-check-input" checked>
+                    <label for="start_now" class="form-check-label">Start now?</label>
+                </div>
+            
+            <button type="submit" class="btn btn-success">Create game</button>
+            </div>
         </div>
     </div>
+
+</div>
+
 
     <!-- Disable for MVP
     <p>
@@ -86,14 +119,6 @@
     </p>
     -->
 
-<div class="form-row">
-    <div class="form-group col-md-12">
-        <input type="checkbox" name="start_now" id="start_now" class="form-check-input" checked>
-        <label for="start_now" class="form-check-label">Start now?</label>
-    </div>
-</div>
-    
-    <button type="submit" class="btn btn-success">Create game</button>
 </form>
 
     {{-- Return form errors --}}

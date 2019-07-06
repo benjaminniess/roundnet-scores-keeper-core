950c4193777c2bd4758b72141c716dde4771b06b
Benjamin Niess <benjamin.niess@gmail.com>
2019-07-06 14:57:09 +0200

    Added a meta description for SEO

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Save your Spikeball / Roundnet scores history and play againsts your friends" />
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>

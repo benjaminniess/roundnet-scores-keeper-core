6d10c10acd38bcf07cf61964c1a95f95cd9863d5
Benjamin Niess <benjamin.niess@gmail.com>
2019-07-06 18:04:04 +0200

    tried to add action types crud

---
+++ app/Http/Controllers/Admin/ActionTypeCrudController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+
+// VALIDATION: change the requests to match your own file names if you need form validation
+use App\Http\Requests\ActionTypeRequest as StoreRequest;
+use App\Http\Requests\ActionTypeRequest as UpdateRequest;
+use Backpack\CRUD\CrudPanel;
+
+/**
+ * Class ActionTypeCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read CrudPanel $crud
+ */
+class ActionTypeCrudController extends CrudController
+{
+    public function setup()
+    {
+        /*
+        |--------------------------------------------------------------------------
+        | CrudPanel Basic Information
+        |--------------------------------------------------------------------------
+        */
+        $this->crud->setModel('App\Models\ActionType');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/actiontype');
+        $this->crud->setEntityNameStrings('actiontype', 'action_types');
+
+        $this->crud->setColumns(['name', 'action_type']);
+        $this->crud->addField([
+            'name' => 'name',
+            'type' => 'text',
+            'label' => "Name"
+        ]);
+        $this->crud->addField([
+            'name' => 'action_type',
+            'type' => 'text',
+            'label' => "Action type"
+        ]);
+    }
+
+    public function store(StoreRequest $request)
+    {
+        return parent::storeCrud();
+    }
+
+    public function update(UpdateRequest $request)
+    {
+        return parent::updateCrud();
+    }
+}

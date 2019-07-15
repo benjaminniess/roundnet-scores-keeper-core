be0a8781462a88096daade05387bdb2d267eaae4
Chly <charley.guenier@gmail.com>
2019-07-15 15:14:23 +0200

    Remake main nav

--- resources/views/components/main-nav.blade.php
+++ resources/views/components/main-nav.blade.php
@@ -1,4 +1,4 @@
-<div class="links">
+{{-- <div class="links">
     <a href="/">Home</a>
     @guest
     <a href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -12,4 +12,35 @@
         <a href="/user/account">Account</a>
         <a href="/logout">Logout</a>
     @endguest
-</div>
+</div> --}}
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+    </li>
+    @guest
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+    </li>
+    @if (Route::has('register'))
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+    </li>
+    @endif
+    @else
+    <li class="nav-item">
+        <a class="nav-link" href="/games">Games</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/friends">Friends</a>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="/user/stats">My stats</a>
+            <a class="dropdown-item" href="/user/account">My profil</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="/logout">Logout</a>
+        </div>
+    </li>
+    @endguest
+</ul>

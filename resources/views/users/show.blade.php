44b6ce36a65e9e3a07fc75fe94ac99e4309817fa
Chly <charley.guenier@gmail.com>
2019-08-03 16:56:54 +0200

    Fix badges displaying issues

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -32,26 +32,21 @@
     	</div>
     	<div class="col-md-4">
     		<div class="card">
-			  <div class="card-header">
-			    badges
-			  </div>
-				  <div class="list-group">
-				  	@foreach ($badges as $badge)
-						<div class="list-group-item list-group-item-action flex-column align-items-start">
-							<div class="d-flex w-100 justify-content-between">
-								<h5 class="mb-1">{{ $badge->name }}</h5>
-								<small class="text-muted">{{ $badge->pivot->created_at->diffForHumans() }}</small>
-							</div>
-								<p class="mb-1">{{ $badge->description }}</p>
-							</div>
-						</div>
-				  	@endforeach
-			  {{-- <ul class="list-group list-group-flush">
+				<div class="card-header">
+					badges
+				</div>
+				<div class="list-group">
 			  	@foreach ($badges as $badge)
-			  		<li class="list-group-item"><strong> {{ $badge->name }} </strong></li>
-			  	@endforeach
-			  </ul> --}}
-			</div>	    
+					<div class="list-group-item list-group-item-action flex-column align-items-start">
+						<div class="d-flex w-100 justify-content-between">
+							<h5 class="mb-1">{{ $badge->name }}</h5>
+							<small class="text-muted">{{ $badge->pivot->created_at->diffForHumans() }}</small>
+						</div>
+							<p class="mb-1">{{ $badge->description }}</p>
+					</div>
+					@endforeach
+				</div>
+			</div>
     	</div>
     </div>
 
